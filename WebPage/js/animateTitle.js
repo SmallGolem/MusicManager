586a3d45a739930cbf3a252efddf2e5335491832
Utilisateur <Utilisateur@DESKTOP-CJMERHO.home>
2021-05-11 14:21:48 +0200

    indev

--- WebPage/js/animateTitle.js
+++ WebPage/js/animateTitle.js
@@ -1,12 +1,15 @@
-var text = ["Music Manager","The technologie for the music"];
+var text = ['Music Manager','The technologie for the music'];
 var size = 30;
 var police = null;
 var cooldown = 0.25;
-var text_cooldown = 1.75;
-var letter_cooldown = 0.2
+var text_cooldown = 1;
+var letter_cooldown = 0.02;
 
-var title = document.getElementById(title);
-var title = null
+
+var title = document.getElementById('title');
+var title_text = title.textContent;
+let text_number;
+let letter_number;
 
 function wait(milliseconds) {
   var start = new Date().getTime();
@@ -17,17 +20,27 @@ function wait(milliseconds) {
   }
 }
 
-function titleanim (text, text_cooldown, letter_cooldown) {
-	while (true) {
-		for (let i = 0; i < text.length; i++) {
-			var letters = text[i].split("");
-			for (let i = 0; i < letters.length; i++) {
-	  			//console.log(letters[i]);
-	 			var title = title + letters[i];
-	  			wait(letter_cooldown * 1000);
-			}
-			wait(text_cooldown * 1000);
-			var title = null;
-		}
+function write() {
+	
+	title.innerHTML = "";
+	var text_number =+ 1;
+	if (text_number > text.length) var text_number = 0;
+	
+	for (i = 0; i < text[text_number].split(""); i++ ) {
+    text.innerHTML += "<span hidden>" + text[text_number].split("")[i] + "</span>";
 	}
+	
 }
+
+function showLetter (){
+	letters = text[text_number].split("");
+}
+
+function onTick (text) {
+	letters = text[text_number].split("");
+	if (letter_number > letters.length) write();
+	showLetter();
+		
+}
+
+setInterval(onTick(text), 0);
